fa313c0324156b66c5b876ccd5b0bd8005e6bff6
anurag <sharma.anurag2k4@gmail.com>
2020-09-09 21:19:24 +0530

    1st commit of CaseDaily.scala

--- src/main/scala/processing/CaseDaily.scala
+++ src/main/scala/processing/CaseDaily.scala
@@ -1,5 +1,25 @@
 package processing
 
+import utils.Utilities
+import org.apache.spark.sql.functions.{col, date_format}
 object CaseDaily {
+
+  def main (args : Array[String]) : Unit = {
+
+    val spark = Utilities.createSparkSession("Daily case landing to main processing")
+
+    //Processing date
+    val process_date = "2020-04-20"
+
+    // Reading data from cassandra tables
+
+    val case_df = spark.read
+      .format("org.apache.spark.sql.cassandra")
+      .options(Map("keyspace" -> "edureka_735821", "table" -> "case_daily"))
+      .load()
+      .filter( date_format(col("create_timestamp"), "yyyy-MM-dd") === process_date)
+
+    Utilities.loadDB(case_df, "CASE_DAILY")
+  }
 
 }
