fe485523c65a69c628d22c46c599b773448a5448
anurag <sharma.anurag2k4@gmail.com>
2020-09-13 23:57:51 +0530

    1st commit of CaseCategory.scala

---
+++ src/main/scala/sources/database/CaseCategory.scala
@@ -0,0 +1,56 @@
+package sources.database
+
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import utils.Utilities
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+
+object CaseCategory {
+
+  def main (args : Array[String]) : Unit = {
+
+    val spark = Utilities.createSparkSession("Case category dimension warehouse loading")
+
+    val schema = new StructType(Array(
+      StructField("category_key", StringType, false),
+      StructField("sub_category_key", StringType, false),
+      StructField("category_description", StringType, false),
+      StructField("sub_category_description", StringType, false),
+      StructField("priority", StringType, false)
+    ))
+
+    //Defining a file system
+    val fs : FileSystem = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+
+    var priority : DataFrame = null
+
+    // Reading data from hdfs with correct schema
+    if (args.length == 1 && fs.exists(new Path(args(0)))){
+      priority = readData(spark, args(0), schema)
+    } else {
+      priority = readData(session = spark, schm = schema)
+    }
+
+    //Loading data to dimension
+    if (args.length == 2){
+      loadData( df = priority, tableName = args(1))
+    } else {
+      loadData( df = priority)
+    }
+  }
+
+  /*Function to read data from hdfs.
+  This makes it possible to read from default location as well as change input location just by adding an argument at run time*/
+  def readData( session : SparkSession,
+                hdfsLoc : String = "/bigdatapgp/common_folder/project_futurecart/batchdata/futurecart_case_category_details.txt",
+                schm : StructType) : DataFrame = {
+
+    Utilities.readDimData(session, hdfsLoc, schm)
+
+  }
+  /*Function to load data from to RDBMS.
+    This makes it possible to load to default table as well as change target table just by adding an argument at run time*/
+  def loadData( df : DataFrame, tableName : String = "DIM_CASE_CATEGORY") = {
+    Utilities.loadDB(df, tableName)
+  }
+}
