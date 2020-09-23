e49a364770ca86f549b16510f8846cae4f0c81a2
anurag <sharma.anurag2k4@gmail.com>
2020-09-23 14:30:26 +0530

    Final commit

--- src/main/scala/connection/Cassandra.scala
+++ src/main/scala/connection/Cassandra.scala
@@ -1,5 +1,29 @@
 package connection
 
+import com.datastax.driver.core.{Session, Cluster}
+
 class Cassandra {
+
+  private var session : Session = null
+  private var cluster : Cluster = null
+
+  def this(node : String, port : Int, keyspace : String = "edureka_735821") {
+    this()
+    this.cluster = Cluster.builder()
+      .addContactPoint(node)
+      .withPort(port)
+      .withCredentials("edureka_735821", "edureka_735821iyktd")
+      .build()
+
+    this.session = cluster.connect(keyspace)
+  }
+
+  def getSession() : Session = {
+    this.session
+  }
+
+  def close() : Unit = {
+    this.session.close()
+  }
 
 }
